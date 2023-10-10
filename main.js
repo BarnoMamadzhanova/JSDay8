48215b61da5960bd7047801518a0a3b45d8e9706
BarnoMamadzhanova <b@mamadzhanova.com>
2023-10-10 15:07:10 +0200

    adding plus and caltotal functions

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-// let flowers = [];
 
 let flowersArr = [];
 class Flowers {
@@ -12,22 +11,22 @@ class Flowers {
     }
 };
 
-const fl1 = new Flowers("Rose", "https://cdn.pixabay.com/photo/2017/03/31/15/32/roses-2191636_1280.jpg", "0", "5€");
-const fl2 = new Flowers("Lily", "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_1280.jpg", "0", "10€");
-const fl3 = new Flowers("Orchid", "https://cdn.pixabay.com/photo/2023/02/26/20/44/flower-7816991_1280.jpg", "0", "25€");
-const fl4 = new Flowers("Dahlia", "https://cdn.pixabay.com/photo/2016/09/04/12/11/dahlia-1643808_1280.jpg", "0", "15€");
-const fl5 = new Flowers("Calla", "https://cdn.pixabay.com/photo/2020/03/08/17/41/flowers-4913165_1280.jpg", "0", "10€");
-const fl6 = new Flowers("Iris", "https://cdn.pixabay.com/photo/2020/02/17/20/26/nettled-iris-4857753_1280.jpg", "0", "8€");
-const fl7 = new Flowers("Sunflower", "https://cdn.pixabay.com/photo/2016/05/28/05/37/sunflower-1421011_1280.jpg", "0", "5€");
-const fl8 = new Flowers("Lilac", "https://cdn.pixabay.com/photo/2017/05/13/20/29/lilac-2310561_1280.jpg", "0", "8€");
-const fl9 = new Flowers("Hydrangea", "https://cdn.pixabay.com/photo/2016/07/07/15/58/hydrangea-1502613_1280.jpg", "0", "30€");
-const fl10 = new Flowers("Anemone", "https://cdn.pixabay.com/photo/2023/03/28/19/01/flower-7883961_1280.jpg", "0", "4€");
-const fl11 = new Flowers("Aster", "https://cdn.pixabay.com/photo/2018/10/18/12/08/aster-3756219_1280.jpg", "0", "4€");
-const fl12 = new Flowers("Peony", "https://cdn.pixabay.com/photo/2015/06/27/17/23/flowers-823655_1280.jpg", "0", "20€");
+const fl1 = new Flowers("Rose", "https://cdn.pixabay.com/photo/2017/03/31/15/32/roses-2191636_1280.jpg", "1", "5");
+const fl2 = new Flowers("Lily", "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_1280.jpg", "1", "10");
+const fl3 = new Flowers("Orchid", "https://cdn.pixabay.com/photo/2023/02/26/20/44/flower-7816991_1280.jpg", "1", "25");
+const fl4 = new Flowers("Dahlia", "https://cdn.pixabay.com/photo/2016/09/04/12/11/dahlia-1643808_1280.jpg", "1", "15");
+const fl5 = new Flowers("Calla", "https://cdn.pixabay.com/photo/2020/03/08/17/41/flowers-4913165_1280.jpg", "1", "10");
+const fl6 = new Flowers("Iris", "https://cdn.pixabay.com/photo/2020/02/17/20/26/nettled-iris-4857753_1280.jpg", "1", "8");
+const fl7 = new Flowers("Sunflower", "https://cdn.pixabay.com/photo/2016/05/28/05/37/sunflower-1421011_1280.jpg", "1", "5");
+const fl8 = new Flowers("Lilac", "https://cdn.pixabay.com/photo/2017/05/13/20/29/lilac-2310561_1280.jpg", "0", "8");
+const fl9 = new Flowers("Hydrangea", "https://cdn.pixabay.com/photo/2016/07/07/15/58/hydrangea-1502613_1280.jpg", "1", "30");
+const fl10 = new Flowers("Anemone", "https://cdn.pixabay.com/photo/2023/03/28/19/01/flower-7883961_1280.jpg", "1", "4");
+const fl11 = new Flowers("Aster", "https://cdn.pixabay.com/photo/2018/10/18/12/08/aster-3756219_1280.jpg", "1", "4");
+const fl12 = new Flowers("Peony", "https://cdn.pixabay.com/photo/2015/06/27/17/23/flowers-823655_1280.jpg", "1", "20");
 
-function display(cards) {
+function display(flowers) {
     document.getElementById("display").innerHTML = "",
-    cards.forEach((value)=> {
+    flowers.forEach((value)=> {
         document.getElementById("display").innerHTML += `
 
         <div class="card-container my-4">
@@ -36,15 +35,13 @@ function display(cards) {
                 <div class="product-img">
                     <img src="${value.image}" alt="${value.name}">
                         <div class="product-cart">
-                            <button><i class="bi bi-plus-square-fill"></i></button>
-                            <button><i class="bi bi-dash-circle-fill"></i></button>
-                            <button><i class="bi bi-bag-heart-fill"></i></i></button>
+                            <button class="addToCart"><i class="bi bi-bag-heart-fill"></i></button>
                         </div>
                     </div> 
                 <div class="product-info">
                         <p class="lead">${value.name}</p>
                         <p class="price text-center m-0">
-                            <span>${value.price}</span>
+                            <span>${value.price}€</span>
                         </p>
                 </div>
                 </div>     
@@ -56,4 +53,74 @@ function display(cards) {
 
 display(flowersArr);
 
+let flowersCart = [];
 
+let addBtns = document.querySelectorAll(".addToCart");
+
+addBtns.forEach(function(btn, i){
+    btn.addEventListener("click", function(){
+        addToCart(i);
+    })
+});
+
+function addToCart(i){
+    if(flowersCart.find((item) => item.name == flowersArr[i].name)){
+        flowersArr[i].quantity++;
+    } else {
+        flowersCart.push(flowersArr[i]);
+    }
+    createCartInHTML();
+    calcTotal();
+}
+
+function createCartInHTML(){
+    document.getElementById("cart").innerHTML = "";
+    flowersCart.forEach(function(value){
+        document.getElementById("cart").innerHTML = `
+        <div class="card-container my-4">
+            <div class="card">
+             <div class="product-details"> 
+                <div class="product-img">
+                    <img src="${value.image}" alt="${value.name}">
+                        <div class="product-cart">
+                            <button class="plus"><i class="bi bi-plus-square-fill"></i></button>
+                            <button class="minus"><i class="bi bi-dash-circle-fill"></i></button>
+                            <button class="quantity"><div >${value.quantity}</div></button>
+                            <button class="close"><i class="bi bi-x-circle-fill"></i></button>
+                        </div>
+                </div> 
+                <div class="product-info">
+                        <p class="lead">${value.name}</p>
+                        <p class="price text-center m-0">
+                            <span>${value.price}€</span>
+                        </p>
+                </div >
+             </div>   
+            </div> 
+        `
+    })
+
+    let plusBtns = document.querySelectorAll(".plus");
+    plusBtns.forEach((btn, i) => {
+        btn.addEventListener("click", function(){
+            plusQuantity(i);
+            calcTotal();
+        })
+    })
+
+
+}
+
+function plusQuantity(i) {
+    flowersCart[i].quantity++;
+    document.querySelectorAll(".quantity")[i].innerText = flowersCart[i].quantity;
+}
+
+function calcTotal(){
+    let total = 0;
+
+    flowersCart.forEach(function(flower){
+        total = total + (flower.price * flower.quantity);
+    })
+    document.getElementById("total").innerHTML = total + "€";
+}
